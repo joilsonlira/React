7a285fd52c7782033954f8e90d37dbc2d69c3efd
Joilson Lira <joilson.lira@live.com>
2020-11-21 19:32:27 -0300

    Comentarios da pagina index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import './index.css'
-import React from 'react'
-import reactDOM from 'react-dom'
-import App from './App.jsx'
+import './index.css' //importa o estilo CSS pa a pagina index
+
+import React from 'react' //importa a biblioteca React
+
+import reactDOM from 'react-dom' // importa o renderizador react
+
+import App from './App.jsx' //importa o componente App
 
 
 
 reactDOM.render(
-    <App></App>,
+    <App></App>, //componente App
     document.getElementById('root')
 );
